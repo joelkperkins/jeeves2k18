d77bd20622c608315bb7655e7503fa9382646bf3
Warlock <joel.climbs@gmail.com>
2018-09-27 16:57:54 -0700

    we got the POST! now lets read the body

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/',
-  (req, res) => console.log(('Hello, I am Jeeves!')));
+  (req, res) => console.log(('Hello, I am Jeeves! I just got this POST: ', req.body)));
 
 app.listen(port, () => console.log(`Jeeves is on port ${port}!`));
