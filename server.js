0511741c0db617b1189033e1569e4b550d83242d
Warlock <joel.climbs@gmail.com>
2018-09-27 14:14:35 -0700

    testing to see if server responds to a POST request

--- server.js
+++ server.js
@@ -5,13 +5,8 @@ const EventEmitter = require('events');
 http.createServer((request, response) => {
   const SEARCH = 'filter';
   console.log(request.url);
-  if (request.method === 'POST' && request.url === `/?url=https://www.googleapis.com/%0A%20%20customsearch/v1?key=AIzaSyBxosEOVRALmuCV-TzhLjfroQuhd8ex9Sk%26cx=cx=006754435674325922743:eel7glah3ve%26q=${SEARCH}`) {
-    fs.readFile('./server/index.html', (err, data) => {
-      if (err) return console.error(err);
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.write(data);
-      response.end(console.log(data));
-    });
+  if (request.method === 'POST') {
+    console.log('Jeeves is here.');
   }
 }).listen(3000);
 
