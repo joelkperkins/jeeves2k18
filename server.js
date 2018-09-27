583ca4fb4060a7b0ea6812bb871a60b84365ec56
Warlock <joel.climbs@gmail.com>
2018-09-27 16:39:06 -0700

    okay express, works...now body-parser

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('handle', (req, res) => console.log(('Hello, I am Jeeves!')));
+app.post((req, res) => console.log(('Hello, I am Jeeves!')));
 
 app.listen(port, () => console.log(`Jeeves is on port ${port}!`));
