9ad60716326bc8be586c7b5ac8795521fb98c20e
Warlock <joel.climbs@gmail.com>
2018-09-27 15:44:39 -0700

    trying to parse out the body of the request using vanilla node

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ http.createServer((request, response) => {
   console.log(request.url);
   if (request.method === 'POST') {
     console.log('Jeeves is here.');
-    console.log('Jeeves is looking for: ', request.body);
+    let body = '';
+    request.on('data', chunk => body += chunk);
+    request.on('end', () => {
+      console.log('Jeeves is looking for: ', body);
+    });
     response.end(JSON.parse('Hello'));
   }
 }).listen(process.env.PORT || 3000);
