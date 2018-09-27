cc0af0d7bacd5ced7df91f5e25189f0c2bef8dec
Warlock <joel.climbs@gmail.com>
2018-09-27 16:08:29 -0700

    okay I surrender, lets use express

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-const http = require('http');
-const fs = require('fs');
-const EventEmitter = require('events');
+const express = require('express');
 
-http.createServer((request, response) => {
-  const SEARCH = 'filter';
-  console.log(request.url);
-  if (request.method === 'POST') {
-    console.log('Jeeves is here.');
-    let body = '';
-    request.on('data', chunk => body += chunk);
-    request.on('end', () => {
-      console.log('Jeeves is looking for: ', body);
-    });
-    response.end(JSON.parse('Hello'));
-  }
-}).listen(process.env.PORT || 3000);
+const app = express();
+const port = process.env.PORT;
+let bodyParser = require('body-parser');
 
-console.log(`Jeeves is listening on ${process.env.PORT} or 3000`);
+app.post((req, res) => console.log(('Hello, I am Jeeves!')));
+
+app.listen(port, () => console.log(`Jeeves is on port ${port}!`))
+;
