80c7c4b1ceb231d9238538fa0ec861e1196abb84
Warlock <joel.climbs@gmail.com>
2018-09-28 01:07:32 -0700

    installing fetch stuff

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -11,7 +12,7 @@ app.post('/',
   (req, res) => {
     console.log('Hello, I am Jeeves! I just got this POST: ', req.body.result.resolvedQuery);
     fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBxosEOVRALmuCV-TzhLjfroQuhd8ex9Sk&cx=006754435674325922743:eel7glah3ve&q=${SEARCH}`)
-      .then(data => data.json)
+      .then(data => data.json())
       .then(data => console.log(data));
   });
 
