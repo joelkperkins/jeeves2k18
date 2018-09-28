bfe4ca3650dcd3e246839f41aaa946bbf7389ac6
Warlock <joel.climbs@gmail.com>
2018-09-28 00:59:24 -0700

    same as last one

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ app.use(bodyParser.json());
 app.post('/',
   (req, res) => {
     console.log('Hello, I am Jeeves! I just got this POST: ', req.body.result.resolvedQuery);
-      fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBxosEOVRALmuCV-TzhLjfroQuhd8ex9Sk&cx=006754435674325922743:eel7glah3ve&q=${SEARCH}`)
-        .then((data) => data.json)
-        .then((data) => console.log(data));
-}
-
+    fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBxosEOVRALmuCV-TzhLjfroQuhd8ex9Sk&cx=006754435674325922743:eel7glah3ve&q=${SEARCH}`)
+      .then(data => data.json)
+      .then(data => console.log(data));
+  });
 
 
 app.listen(port, () => console.log(`Jeeves is on port ${port}!`));
