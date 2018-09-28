422996eebdc16c6d0823b38ea878c2af250374c0
Warlock <joel.climbs@gmail.com>
2018-09-28 01:10:32 -0700

    installing fetch stuff

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ app.use(bodyParser.json());
 app.post('/',
   (req, res) => {
     console.log('Hello, I am Jeeves! I just got this POST: ', req.body.result.resolvedQuery);
+    const SEARCH = req.body.result.resolvedQuery;
     fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBxosEOVRALmuCV-TzhLjfroQuhd8ex9Sk&cx=006754435674325922743:eel7glah3ve&q=${SEARCH}`)
       .then(data => data.json())
-      .then(data => console.log(data));
+      .then(data => console.log('This is Jeeves, I just parsed this data:', data));
   });
 
 
