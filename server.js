1e32169a29cc1135a32fb7d013f7a2cf389e57a0
Warlock <joel.climbs@gmail.com>
2018-09-28 00:57:54 -0700

    same as last one

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.post('/',
   (req, res) => {
-    console.log(('Hello, I am Jeeves! I just got this POST: ', req.body.result.resolvedQuery)));
+    console.log('Hello, I am Jeeves! I just got this POST: ', req.body.result.resolvedQuery);
       fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBxosEOVRALmuCV-TzhLjfroQuhd8ex9Sk&cx=006754435674325922743:eel7glah3ve&q=${SEARCH}`)
         .then((data) => data.json)
         .then((data) => console.log(data));
