606b54f4045e7ca864ebbfe98bf3058586b3caf8
Warlock <joel.climbs@gmail.com>
2018-09-30 13:36:57 -0700

    server is running, body parsing is function. looking for clues

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.post('/',
   (req, res) => {
-    console.log('Hello, I am Jeeves! I just got this POST: ', req.body.result.metadata);
+    console.log('Hello, I am Jeeves! I just got this POST: ', req.body.result.fulfillment);
     const SEARCH = req.body.result.resolvedQuery;
     fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBxosEOVRALmuCV-TzhLjfroQuhd8ex9Sk&cx=006754435674325922743:eel7glah3ve&q=${SEARCH}`)
       .then(data => data.json())
